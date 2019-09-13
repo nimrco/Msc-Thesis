0d5abb171d370e7445aef2b3c041e93de6fe41eb
nimrco <nimrco@post.bgu.ac.il>
2019-09-13 15:35:56 +0300

    added debug code

--- downloadRefFiles.py
+++ downloadRefFiles.py
@@ -1,8 +1,31 @@
 from ftplib import FTP
 import os
+import sys
+import signal
+import traceback
+import code
 import parseData
 
 
+def debug(sig, frame):
+    """Interrupt running process, and provide a python prompt for
+    interactive debugging."""
+    d={'_frame':frame}         # Allow access to frame object.
+    d.update(frame.f_globals)  # Unless shadowed by global
+    d.update(frame.f_locals)
+
+    i = code.InteractiveConsole(d)
+    message  = "Signal received : entering python shell.\nTraceback:\n"
+    message += ''.join(traceback.format_stack(frame))
+    i.interact(message)
+
+
+def listen():
+    if sys.platform == "linux" or sys.platform == "linux2":
+        signal.signal(signal.SIGUSR1, debug)  # Register handler
+
+
+listen()
 strains_list_full_path = []
 strains_list = []
 with open("assembly_summary.txt") as parent_dir:
