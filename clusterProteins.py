7aa1ad2ad2b086f5a12c6998944af69aa69907c5
nimrco <nimrco@post.bgu.ac.il>
2019-11-14 17:05:35 +0200

    added input to cd-hit

---
+++ clusterProteins.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+
+seq_list = []
+root = "data/"
+strains = os.listdir(root)
+strains_df = pd.DataFrame(columns=["strain"])
+for index, strain in enumerate(strains):
+    strains_df.loc[len(strains_df)] = [strain]
+    genes = pd.read_csv(root + strain + "/genes.csv")
+    for seq_index, row in genes.iterrows():
+        header = str(index) + "|" + str(seq_index)
+        protein = row["protein"]
+        seq_list.append(">{}\n{}".format(header, protein))
+
+strains_df.index.name = 'index'
+strains_df.to_csv("strains_list.csv")
+
+with open("protein_seq.fasta", "w") as file:
+    file.write("\n".join(seq_list))
