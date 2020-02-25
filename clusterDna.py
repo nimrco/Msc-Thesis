26c785fcb324f91dd9025124205d52a934406f3f
Nimrod <nimrco@post.bgu.ac.il>
2020-02-25 20:39:07 +0200

    changed to store dna on memory

--- clusterDna.py
+++ clusterDna.py
@@ -6,6 +6,10 @@ core_df = pd.read_csv("core_cluster_matrix.csv")
 core_df = core_df[core_df["core_cluster"] == 1].iloc[:, 1:-4]
 strains_df = pd.read_csv("strains_list.csv")
 clusters_df = pd.read_csv("cluster.csv").iloc[:, 1:]
+seq_dict = {}
+
+for strain in os.listdir(root):
+    seq_dict[strain] = pd.read_csv(os.path.join(root, strain, "genes.csv"))["dna"]
 
 for cluster, row in core_df.iterrows():
     seq_list = []
@@ -14,7 +18,8 @@ for cluster, row in core_df.iterrows():
             strain_name = strains_df.iloc[int(strain)]["strain"]
             seq = clusters_df.iloc[cluster][strain]
             seq = seq.strip(" '\'[]")
-            dna = pd.read_csv(os.path.join(root, strain_name, "genes.csv")).iloc[int(seq)]["dna"]
+            dna = seq_dict[strain_name].iloc[int(seq)]
+            # dna = pd.read_csv(os.path.join(root, strain_name, "genes.csv")).iloc[int(seq)]["dna"]
             header = strain + "|" + seq
             seq_list.append(">{}\n{}".format(header, dna))
 
