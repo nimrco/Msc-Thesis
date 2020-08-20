bd04beb000bc5519c058515ee1a1d0f51289feca
Nimrod <nimrco@post.bgu.ac.il>
2020-08-20 19:40:23 +0300

    align with mafft

--- align_core_clusters.py
+++ align_core_clusters.py
@@ -2,8 +2,11 @@ import subprocess
 import os
 
 
-root = "test_dir"
+root = "clusters"
 for cluster in os.listdir(root):
     mafft_args = ["mafft", "--auto", str(os.path.join(root, cluster, cluster + ".fasta"))]
     with open(os.path.join(root, cluster, cluster + "_alignment"), "w") as aligned_file:
         subprocess.run(mafft_args, stdout=aligned_file)
+    print("cluster: {} done".format(cluster))
+
+print("script done")
