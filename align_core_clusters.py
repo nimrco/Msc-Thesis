3510417b7e9e61ff7fb9dfa64b261790e5b0328f
Nimrod <nimrco@post.bgu.ac.il>
2020-08-20 17:09:46 +0300

    testing mafft without stdin

--- align_core_clusters.py
+++ align_core_clusters.py
@@ -3,8 +3,7 @@ import os
 
 
 root = "test_dir"
-mafft_args = ["mafft", "--auto"]
 for cluster in os.listdir(root):
-    with open(os.path.join(root, cluster, cluster + ".fasta")) as cluster_file:
-        with open(os.path.join(root, cluster, cluster + "_alignment"), "w") as aligned_file:
-            subprocess.run(mafft_args, stdin=cluster_file, stdout=aligned_file)
+    mafft_args = ["mafft", "--auto", str(os.path.join(root, cluster, cluster + ".fasta"))]
+    with open(os.path.join(root, cluster, cluster + "_alignment"), "w") as aligned_file:
+        subprocess.run(mafft_args, stdout=aligned_file)
