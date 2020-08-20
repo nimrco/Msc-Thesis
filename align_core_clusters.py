260cc51a15a5e834fc1144b8822fbf468e03d02c
Nimrod <nimrco@post.bgu.ac.il>
2020-08-20 19:44:25 +0300

    added print in start

--- align_core_clusters.py
+++ align_core_clusters.py
@@ -4,6 +4,7 @@ import os
 
 root = "clusters"
 for cluster in os.listdir(root):
+    print("cluster: {} start".format(cluster))
     mafft_args = ["mafft", "--auto", str(os.path.join(root, cluster, cluster + ".fasta"))]
     with open(os.path.join(root, cluster, cluster + "_alignment"), "w") as aligned_file:
         subprocess.run(mafft_args, stdout=aligned_file)
