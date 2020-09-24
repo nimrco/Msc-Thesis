54dae8964d363deaab5c583e41ebda9ff6d1e5b0
Nimrod <nimrco@post.bgu.ac.il>
2020-09-24 20:20:49 +0300

    saved fig

--- genes_pseudo_count.py
+++ genes_pseudo_count.py
@@ -5,13 +5,25 @@ from matplotlib import pyplot as plt
 
 root = "data"
 strains = os.listdir(root)
+genes_list = []
+pseudo_list = []
 strain_dict = {}
-print(os.getcwd())
+index_list = []
 for index, strain in enumerate(strains):
     genes = pd.read_csv(os.path.join(root, strain, "genes.csv"))
     pseudo = pd.read_csv(os.path.join(root, strain, "pseudo_genes.csv"))
     genes_num = len(genes)
     pseudo_num = len(pseudo)
     strain_dict[index] = {"genes": genes_num, "pseudo": pseudo_num}
-    print(genes)
-    print(pseudo)
+    genes_list.append(genes_num)
+    pseudo_list.append(pseudo_num)
+    index_list.append(index)
+
+plt.plot(index_list, genes_list, color="blue", label="genes")
+plt.plot(index_list, pseudo_list, color="red", label="pseudo")
+plt.xlabel("strain number")
+plt.ylabel("number of genes/pseudogenes")
+plt.title("genes vs psuedogenes")
+plt.legend()
+plt.savefig("genes_vs_pseudo.png")
+plt.show()
