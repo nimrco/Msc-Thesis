e166166eb9cccf4a4cc88734b20e606e40a6e91b
Nimrod <nimrco@post.bgu.ac.il>
2020-09-24 19:16:59 +0300

    added enumerate

--- genes_pseudo_count.py
+++ genes_pseudo_count.py
@@ -4,12 +4,13 @@ from matplotlib import pyplot as plt
 
 
 root = "data"
+strains = os.listdir(root)
 strain_dict = {}
 print(os.getcwd())
-for strain in os.listdir(root):
+for index, strain in enumerate(strains):
     genes = pd.read_csv(os.path.join(root, strain, "genes.csv"))
     pseudo = pd.read_csv(os.path.join(root, strain, "pseudo_genes.csv"))
     genes_num = len(genes)
     pseudo_num = len(pseudo)
-    strain_dict[strain] = {"genes": genes_num, "pseudo": pseudo_num}
+    strain_dict[index] = {"genes": genes_num, "pseudo": pseudo_num}
     print(genes)
