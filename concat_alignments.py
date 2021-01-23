3108986435513e7829b910b36888a70306995d9f
Nimrod <nimrco@post.bgu.ac.il>
2021-01-23 17:52:17 +0200

    fixed id for records

--- concat_alignments.py
+++ concat_alignments.py
@@ -7,7 +7,11 @@ def get_alignment_filtered_dict():
     alignment_dict_local = {}
     for cluster_local in os.listdir(config.clusters):
         with open(os.path.join(config.clusters, cluster_local, cluster_local + "_alignment_filtered_without_gaps")) as alignment_file:
-            alignment_dict_local[cluster_local] = AlignIO.read(alignment_file, "fasta")
+            alignment_local = AlignIO.read(alignment_file, "fasta")
+            for record in alignment_local:
+                record.id = record.id.split('|')[0]
+                record.description = record.id
+            alignment_dict_local[cluster_local] = alignment_local
     print("alignment_dict")
     return alignment_dict_local
 
