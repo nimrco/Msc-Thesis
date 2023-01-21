9781239a8d57d59ceee58069ab4bf3994c243bee
Nimrod <nimrco@post.bgu.ac.il>
2023-01-21 14:44:58 +0200

    added config

--- clusterProteins.py
+++ clusterProteins.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import os
-
+import config
 
 seq_list = []
-root = "data/"
-strains = os.listdir(root)
+strains = os.listdir(config.data)
 strains_df = pd.DataFrame(columns=["strain"])
 for index, strain in enumerate(strains):
-    print(strain)
     strains_df.loc[len(strains_df)] = [strain]
-    genes = pd.read_csv(root + strain + "/genes.csv")
+    genes = pd.read_csv(os.path.join(config.data, strain, "genes.csv"))
     for seq_index, row in genes.iterrows():
         header = str(index) + "|" + str(seq_index)
         protein = row["protein"]
@@ -20,4 +18,4 @@ strains_df.to_csv("strains_list.csv")
 
 with open("protein_seq.fasta", "w") as file:
     file.write("\n".join(seq_list))
-print("done")
+print("script done")
