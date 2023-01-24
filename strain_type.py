1748fa28c9eaf48c5d570dfaa4b9a694583ed6c8
Nimrod <nimrco@post.bgu.ac.il>
2023-01-24 18:54:37 +0200

    added number of genes and pseudogenes

--- strain_type.py
+++ strain_type.py
@@ -1,9 +1,69 @@
 import os
 import pandas as pd
 import multiprocessing as mp
-from cluster_analysis import get_cluster_distribution
-from mlst import get_mlsts
 import config
+
+
+def get_mlsts(groupby_flag=True):
+    mlst_data = pd.read_excel(os.path.join(config.tables, "strain_summary.xlsx"))
+    mlst_data = mlst_data[['Species', 'Strain', 'Refseq assembly accession', 'MLST Sequence Type']]
+    mlst_data = mlst_data[mlst_data['Species'] == 'Pseudomonas aeruginosa']
+    mlst_data = mlst_data.dropna(subset=['MLST Sequence Type'])
+    mlst_data['MLST'] = mlst_data['MLST Sequence Type'].apply(lambda r: r.split('|')[-1])
+    mlst_data = mlst_data[mlst_data['MLST'] != '-']
+    mlst_data['Refseq'] = mlst_data['Refseq assembly accession'].apply(lambda r: r.split('.')[0])
+    if groupby_flag:
+        mlst_types = mlst_data.groupby('MLST')
+        mlsts = mlst_types.filter(lambda x: len(x) >= 5)
+        return mlsts
+    return mlst_data
+
+
+def get_cluster_distribution():
+    cluster_size = {}
+    cluster_members = 0
+    pseudo_flag = False
+    gene_flag = False
+    gene_counter = 0
+    pseudo_counter = 0
+    cluster = None
+    with open(os.path.join(config.seq_files, "repr", "combined_clusters.clstr")) as cluster_file:
+        for line in cluster_file:
+            if line.startswith(">"):
+                temp_cluster = line.split()[1]
+                if cluster:
+                    if pseudo_flag and gene_flag:
+                        cluster_type = "mixed"
+                    elif gene_flag:
+                        cluster_type = "gene"
+                    else:
+                        cluster_type = "pseudo"
+                    cluster_size[cluster] = (cluster_members, cluster_type, gene_counter, pseudo_counter)
+                    cluster = temp_cluster
+                    cluster_members = 0
+                    pseudo_flag = False
+                    gene_flag = False
+                    gene_counter = 0
+                    pseudo_counter = 0
+                else:
+                    cluster = temp_cluster
+            else:
+                cluster_members += 1
+                member_type = line.split(">")[1].split("|")[0]
+                if member_type == 'Gene':
+                    gene_flag = True
+                    gene_counter += 1
+                else:
+                    pseudo_flag = True
+                    pseudo_counter += 1
+    if pseudo_flag and gene_flag:
+        cluster_type = "mixed"
+    elif gene_flag:
+        cluster_type = "gene"
+    else:
+        cluster_type = "pseudo"
+    cluster_size[cluster] = (cluster_members, cluster_type, gene_counter, pseudo_counter)
+    return cluster_size
 
 
 def get_clusters_dict(mode=''):
@@ -27,30 +87,37 @@ def get_clusters_dict(mode=''):
     return clusters_dict
 
 
-def cluster_file_for_tree(cluster):
-    strains_df = pd.read_csv(os.path.join(config.tables, "strains_list_names.csv"))
-    strains_df['gene'] = 0
-    strains_df['pseudo'] = 0
-    strains_df['MLST'] = ''
+def cluster_file_for_tree(cluster, ratio_flag=False):
     gene_cluster = clusters_map.get(cluster)[0]
     gene_members = results_dict[0].get(gene_cluster)
     pseudo_cluster = clusters_map.get(cluster)[2]
     pseudo_members = results_dict[1].get(pseudo_cluster)
     strains = list(gene_members.union(pseudo_members))
-    for strain in strains:
-        if strain in gene_members:
-            strains_df.at[int(strain), 'gene'] = 1
-        if strain in pseudo_members:
-            strains_df.at[int(strain), 'pseudo'] = 1
+    strains_df = pd.read_csv(os.path.join(config.tables, "strains_list_names.csv"))
+    strains_df['MLST'] = ''
+    if ratio_flag:
+        ratio_df = pd.read_csv(os.path.join(config.tables, "supplementary_table.csv"))
+        strains_df['ratio'] = 0
+        for strain in strains:
+            strains_df.at[int(strain), 'ratio'] = ratio_df.at[int(strain), "ratio genes/pseudogenes"]
+    else:
+        count_df = pd.read_csv(os.path.join(config.tables, "genes_pseudogenes_count.csv"))
+        strains_df['gene'] = 0
+        strains_df['pseudo'] = 0
+        for strain in strains:
+            if strain in gene_members:
+                strains_df.at[int(strain), 'gene'] = count_df.at[int(strain), "# genes"]
+            if strain in pseudo_members:
+                strains_df.at[int(strain), 'pseudo'] = count_df.at[int(strain), "# pseudogenes"]
     return strains_df
 
 
 with mp.Pool(processes=2) as pool:
     results_dict = pool.map(get_clusters_dict, ['', '_pseudo'])
 
-
 cluster_size = get_cluster_distribution()
 mlsts = get_mlsts()
+# mixed with one gene and one pseudogene
 cluster_size = {cluster: val for cluster, val in cluster_size.items() if val[0] == 2
                 and val[1] == 'mixed'}
 clusters_map = {}
@@ -83,18 +150,18 @@ with open(os.path.join(config.seq_files, 'repr', 'combined_clusters.clstr')) as 
                                              origin_pseudo_cluster,
                                              origin_pseudo_cluster_size)
 
-clusters_map = {cluster: val for cluster, val in clusters_map.items() if val[1] <= 1500 and val[3] >= 50}
+clusters_map = {cluster: val for cluster, val in clusters_map.items() if val[1] <= 2500 and val[3] >= 2}
 clusters_map = {cluster: val for cluster, val in sorted(clusters_map.items(),
                                                         key=lambda item: item[1][1],
                                                         reverse=True)}
 
-for cluster in list(clusters_map.keys())[:10]:
+for cluster in list(clusters_map.keys()):
     strains_df = cluster_file_for_tree(cluster)
     for index, row in strains_df.iterrows():
         mlst_series = mlsts[mlsts['Strain'] == row['strain']]['MLST']
         if mlst_series.empty:
             continue
         strains_df.at[index, 'MLST'] = mlst_series.values[0]
-    strains_df.to_csv(os.path.join(config.tables, "cluster_{}.csv".format(cluster)), index=False)
+    strains_df.to_csv(os.path.join(config.tables, "clusters", "cluster_{}.csv".format(cluster)), index=False)
 
 print("script done")
